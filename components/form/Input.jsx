721ab19bfff0e12a7ec239294fc1d24ad7314763
İlknur Helvacı <74411042+ilknurhelvaci@users.noreply.github.com>
2024-11-16 22:01:29 +0300

    added reservation component and reservation validation schema

--- components/form/Input.jsx
+++ components/form/Input.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 const Input = (props) => {
-  const { type, placeholder, ...inputProps } = props;
+  const { type, errorMessage, touched, placeholder, ...inputProps } = props;
 
   const [hasValue, setHasValue] = useState(false);
 
@@ -14,28 +14,49 @@ const Input = (props) => {
   };
 
   return (
-    <div className="relative w-full">
-      <input
-        type={type}
-        id="input"
-        className="peer p-3 pt-5 pb-2 border border-primary focus:outline-none focus:ring-0  w-full"
-        placeholder=" "
-        required
-        {...inputProps}
-        onChange={handleInputChange}
-      />
+    // <div className="relative w-full">
+    //   <input
+    //     type={type}
+    //     id="input"
+    //     className="peer p-3 pt-5 pb-2 border border-primary focus:outline-none focus:ring-0  w-full"
+    //     placeholder=" "
+    //     required
+    //     {...inputProps}
+    //     // onChange={handleInputChange}
+    //   />
 
-      <label
-        htmlFor="input"
-        className={`absolute left-3 top-1/2 transform 
-        -translate-y-1/2 text-gray-500 transition-all 
-          peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm
-          peer-placeholder-shown:text-gray-500 peer-focus:top-0 peer-focus:text-xs peer-focus:text-gray-500 bg-white 
-        
+    //   <label
+    //     htmlFor="input"
+    //     className={`absolute left-3 top-1/2 transform
+    //     -translate-y-1/2 text-gray-500 transition-all
+    //       peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm
+    //       peer-placeholder-shown:text-gray-500 peer-focus:top-0 peer-focus:text-xs peer-focus:text-gray-500 bg-white
+
+    //       `}
+    //   >
+    //     {placeholder}
+    //   </label>
+    // </div>
+
+    <div className="w-full">
+      <label className="relative block cursor-text w-full">
+        <input
+          type={type}
+          className={`h-14 w-full border outline-none px-4 peer 
+          ${type !== "datetime-local" && "pt-2"}
+          ${touched && errorMessage ? "border-red-500" : "border-primary"}
+          
           `}
-      >
-        {placeholder}
+          required
+          {...inputProps}
+        />
+        {type !== "datetime-local" && (
+          <span className="absolute top-0 left-0 px-4 text-sm flex items-center h-full peer-focus:h-7 peer-focus:text-xs peer-valid:h-7 peer-valid:text-xs transition-all">
+            {placeholder}
+          </span>
+        )}
       </label>
+      {touched && <span className="text-xs text-danger">{errorMessage}</span>}
     </div>
   );
 };
--- components/ui/Reservation.jsx
+++ components/ui/Reservation.jsx
@@ -1,51 +1,93 @@
 import React from "react";
 import Title from "./Title";
 import Input from "../form/Input";
+import { useFormik } from "formik";
+import { reservationSchema } from "@/schema/reservation";
 const Reservation = () => {
+  const onSubmit = async (values, actions) => {
+    // formda reset işleminden önce için 4 sn bekletme
+
+    await new Promise((resolve) => setTimeout(resolve, 4000));
+    actions.resetForm();
+  };
+  const { values, handleSubmit, handleChange, handleBlur, touched, errors } =
+    useFormik({
+      initialValues: {
+        fullName: "",
+        phoneNumber: "",
+        email: "",
+        persons: "",
+        date: "",
+      },
+      onSubmit,
+      validationSchema: reservationSchema,
+    });
   const inputs = [
     {
       id: 1,
-      name: "fullaName",
+      name: "fullName",
       type: "text",
       placeholder: "Your Full Name",
+      value: values.fullName,
+      errorMessage: errors.fullName,
+      touched: touched.fullName,
     },
     {
       id: 2,
       name: "phoneNumber",
       type: "number",
       placeholder: "Your Phone Number",
+      value: values.phoneNumber,
+      errorMessage: errors.phoneNumber,
+      touched: touched.phoneNumber,
     },
     {
       id: 3,
       name: "email",
       type: "email",
       placeholder: "Your Email Address",
+      value: values.email,
+      errorMessage: errors.email,
+      touched: touched.email,
     },
     {
       id: 4,
       name: "persons",
       type: "number",
       placeholder: "How Many Persons?",
+      value: values.persons,
+      errorMessage: errors.persons,
+      touched: touched.persons,
     },
     {
       id: 5,
-      name: "persons",
+      name: "date",
       type: "datetime-local",
-      placeholder: "How Many Persons?",
+      placeholder: "Date",
+      value: values.date,
+      errorMessage: errors.date,
+      touched: touched.date,
     },
   ];
   return (
     <div className="container mx-auto py-12">
       <Title addClass="text-[40px] mb-10">Book A Table</Title>
       <div className="flex justify-between flex-wrap gap-10">
-        <div className="lg:flex-1 w-full">
+        <form className="lg:flex-1 w-full" onSubmit={handleSubmit}>
           <div className="flex flex-col gap-y-3">
             {inputs.map((input, index) => (
-              <Input key={index} {...input} />
+              <Input
+                key={index}
+                {...input}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
             ))}
           </div>
-          <button className="btn-primary mt-4">BOOK NOW</button>
-        </div>
+          <button className="btn-primary mt-4" type="submit">
+            BOOK NOW
+          </button>
+        </form>
         <div className="lg:flex-1  w-full">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12045.070502076922!2d28.8402562604011!3d40.99751518663962!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14caa4af25e14c1d%3A0x75e589dbfab0084f!2zQmFow6dlbGlldmxlciwgxZ5pcmluZXZsZXIsIDM0MTAwIEJhaMOnZWxpZXZsZXIvxLBzdGFuYnVs!5e0!3m2!1str!2str!4v1731757360026!5m2!1str!2str"
